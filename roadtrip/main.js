332cfe4b9b07db71d411871b54d6caf3e866b301
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-06 21:35:08 -0600

    Made items into components

--- roadtrip/main.js
+++ roadtrip/main.js
@@ -1,4 +1,36 @@
-var mainAppVm = new Vue({
+var item = Vue.component( 'item', {
+    template: `
+                <div class="col-md-4">
+                    <h2>{{item.name}}</h2>
+                    <img class="img-responsive" v-bind:src="item.imagePath"/>
+                    <h3>Weight: {{item.weight}} lbs</h3>
+                    <button class="btn btn-success" v-on:click="buyItem(item)">Buy - {{item.price }}</button>
+                    <button class="btn btn-danger" v-on:click="sellItem(item)">Sell - {{ item.price }}</button>
+                </div>
+              `,
+    data: function ()
+    {
+        return {}
+    },
+    props: ['item'],
+    computed: {
+
+    },
+    methods: {
+        //check to see if not enough money
+        buyItem: function ( item )
+        {
+            this.$emit( 'buy-item', item )
+        },
+        sellItem: function ( item )
+        {
+            this.$emit( 'sell-item', item )
+        }
+    }
+});
+
+
+var mainAppVm = new Vue( {
     el: '#app',
     data: {
         usersMoney: 100,
@@ -9,61 +41,69 @@ var mainAppVm = new Vue({
             {
                 name: "Bazooka",
                 description: "You never know when ",
-                imagePath:'http://baw-weapons.yez.dk/uploads/7/5/5/2/7552490/95044_orig.jpg',
+                imagePath: 'http://baw-weapons.yez.dk/uploads/7/5/5/2/7552490/95044_orig.jpg',
                 price: 15,
-                weight:30
+                weight: 30
             }
-
-
         ]
-
-
     },
 
     computed: {
 
     },
     methods: {
-        
-            //check to see if not enough money
-        buyItem: function (item) {
-            this.isValidPurchase(item);
+
+        buyItem: function (item)
+        {
+            this.isValidPurchase( item );
             this.usersMoney -= item.price;
             this.weightOfVehicle += item.weight;
         },
-        sellItem: function (item) {
-            this.isValidSale(item);
+        sellItem: function ( item )
+        {
+            this.isValidSale( item );
             this.usersMoney += item.price;
             this.weightOfVehicle -= item.weight;
         },
-        isValidPurchase: function (item) {
+        isValidPurchase: function ( item )
+        {
             var isPurchaseValid = true;
-            if (this.isOverWeight(item.weight)){
+            if ( this.isOverWeight( item.weight ) )
+            {
                 isPurchaseValid = false;
             }
         },
-        isValidSale: function (item) {
+        isValidSale: function ( item )
+        {
             var isValidSale = true;
-            if (this.isUnderWeight(item.weight)) {
+            if ( this.isUnderWeight( item.weight ) )
+            {
                 isValidSale = false;
             }
         },
-        isOverWeight: function (weight) {
-            if (this.weightOfVehicle + weight > this.maxWeightOfVehicle) {
-                console.log("over weight limit");
+        isOverWeight: function ( weight )
+        {
+            if ( this.weightOfVehicle + weight > this.maxWeightOfVehicle )
+            {
+                console.log( "over weight limit" );
                 return true;
-            } else {
+            } else
+            {
                 return false;
             }
         },
-        isUnderWeight: function (weight) {
-            if (this.weightOfVehicle - weight < this.minWeightOfVehicle) {
-                console.log("under weight limit");
+        isUnderWeight: function ( weight )
+        {
+            if ( this.weightOfVehicle - weight < this.minWeightOfVehicle )
+            {
+                console.log( "under weight limit" );
                 return true;
-            } else {
+            } else
+            {
                 return false;
             }
         }
-
     }
+
 })
+
