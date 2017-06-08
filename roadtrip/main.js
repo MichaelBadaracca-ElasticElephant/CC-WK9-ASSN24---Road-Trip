a578d4e87f0078a99023c7dad581794695f498e6
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-08 10:19:25 -0600

    Added hellish validation when an invalid transaction happens

--- roadtrip/main.js
+++ roadtrip/main.js
@@ -1,8 +1,8 @@
 var item = Vue.component( 'item', {
     template: `
-                <div class="col-md-3">
+                <div class="item col-md-3">
                     <h2>{{item.name}}</h2>
-                    <img class="img-responsive" v-bind:src="item.imagePath"/>
+                    <img class="item-image img-responsive" v-bind:src="item.imagePath" v-on:click="buyItem(item)"/>
                     <h3>Weight: {{item.weight}} lbs</h3>
                     <button class="btn btn-success" v-on:click="buyItem(item)">Buy: $ {{item.price }}</button>
                     <button class="btn btn-danger" v-on:click="sellItem(item)">Sell: $ {{ item.price }}</button>
@@ -30,14 +30,19 @@ var item = Vue.component( 'item', {
 });
 
 
+
 var mainAppVm = new Vue( {
     el: '#app',
     data: {
         usersMoney: 100,
+        maxUsersMoney: 100,
+        minUserMoney:0,
         weightOfVehicle: 0,
         maxWeightOfVehicle: 500,
         minWeightOfVehicle: 0,
-        validationMessage:"",
+        validationMessage: "STARTING MESSAGE",
+        isValid: true,
+        purchasedItems: [],
         items: [
             {
                 name: "Bazooka",
@@ -50,25 +55,55 @@ var mainAppVm = new Vue( {
                 name: "Spork",
                 description: "You never know when ",
                 imagePath: 'http://cdn.shopify.com/s/files/1/1365/2497/products/tumblr_lyq7rjeyjI1qzfsnio1_500_grande.gif?v=1478473486',
-                price: 50,
+                price: 5,
                 weight: 2
             },
             {
-                name: "Goat with Can",
+                name: "Goat w/ Can",
                 description: "You never know when ",
                 imagePath: 'http://geek-whisperers.com/wp-content/uploads/2015/05/goat_can.jpg',
                 price: 5,
                 weight: 50
             },
             {
-                name: "Pallet of Four Loko with Spirit Guide",
+                name: "4 Loko w/ Bro",
                 description: "You never know when ",
                 imagePath: 'https://rokthespot.files.wordpress.com/2010/11/tumblr_lc5qhnotiw1qc0187o1_500.jpg',
+                price: 2,
+                weight: 200
+            },
+            {
+                name: "Poop Bags",
+                description: "You never know when ",
+                imagePath: 'https://www.fourstarbrand.com/wp-content/uploads/2014/12/poop-bags.jpg',
+                price: 10,
+                weight: 1
+            },
+            {
+                name: "Macho Man",
+                description: "You never know when ",
+                imagePath: 'https://uproxx.files.wordpress.com/2014/11/wlsavage1.jpg?quality=100&w=650',
                 price: 5,
-                weight: 50
+                weight: 250
+            },
+            {
+                name: "Savage Pillow",
+                description: "You never know when ",
+                imagePath: 'https://images-na.ssl-images-amazon.com/images/I/71UAsq6HomL._SX355_.jpg',
+                price: 12,
+                weight: 30
+            },
+            {
+                name: "Fat Pug",
+                description: "You never know when ",
+                imagePath: 'https://pbs.twimg.com/media/BypeAXJIUAE8RKk.jpg',
+                price: 5,
+                weight: 80
             }
         ]
     },
+
+    
 
     computed: {
 
@@ -77,54 +112,53 @@ var mainAppVm = new Vue( {
 
         buyItem: function (item)
         {
-            this.isValidPurchase( item );
             this.usersMoney -= item.price;
             this.weightOfVehicle += item.weight;
+            this.purchasedItems.push( item );
+            this.isValidTransaction();
+
+            
         },
         sellItem: function ( item )
         {
-            this.isValidSale( item );
             this.usersMoney += item.price;
             this.weightOfVehicle -= item.weight;
+            this.isValidTransaction();
         },
-        isValidPurchase: function ( item )
+        isValidTransaction: function ()
         {
-            var isPurchaseValid = true;
-            if ( this.isOverWeight( item.weight ) )
-            {
-                isPurchaseValid = false;
-            }
-            return isPurchaseValid;
-        },
-        isValidSale: function ( item )
-        {
-            var isValidSale = true;
-            if ( this.isUnderWeight( item.weight ) )
-            {
-                isValidSale = false;
-            }
-        },
-        isOverWeight: function ( weight )
-        {
-            if ( this.weightOfVehicle + weight > this.maxWeightOfVehicle )
-            {
-                this.validationMessage = "over weight limit";
-                console.log( "over weight limit" );
-                return true;
-            } else
+            if ( this.isWeightValid() && this.isMoneyValid() )
             {
                 this.validationMessage = "";
+                this.isValid = true;
+            } else
+            {
+                this.isValid = false;
+            }
+            
+        },
+
+        isMoneyValid: function ( )
+        {
+            if ( this.usersMoney >= this.minUserMoney && this.usersMoney <= this.maxUsersMoney )
+            {
+                return true;
+            }
+            else
+            {
+                this.validationMessage = "You're broke fool!"
                 return false;
             }
         },
-        isUnderWeight: function ( weight )
-        {
-            if ( this.weightOfVehicle - weight < this.minWeightOfVehicle )
+
+        isWeightValid: function ( )        {
+            if ( this.weightOfVehicle >= this.minWeightOfVehicle && this.weightOfVehicle <= this.maxWeightOfVehicle )
             {
-                console.log( "under weight limit" );
+                
                 return true;
             } else
             {
+                this.validationMessage = "Too heavy fool!"
                 return false;
             }
         }
